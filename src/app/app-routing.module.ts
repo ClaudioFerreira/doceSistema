8763e7831722ec2facd35ae9b626d803a14638ed
Claudio Ferreira <claudio.zero0@gmail.com>
2022-06-04 18:58:34 -0300

    feat(start project) configuracoes iniciais Criando componentes rotas e guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AuthGuard } from './shared/guards/auth.guard';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent, canActivate: [ AuthGuard ] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
